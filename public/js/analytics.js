690bd5e1b3ec36699b7e60616caf2d1740d679f0
Piotr Luczko <piotr.luczko@gmail.com>
2026-02-15 15:56:56 +0100

    icons

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,3 +1,9 @@
+// Activate preloaded Google Fonts stylesheet
+document.querySelectorAll('link[media="print"][rel="stylesheet"]').forEach(function (link) {
+  link.media = "all";
+});
+
+// Google Analytics (GA4)
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag("js", new Date());
